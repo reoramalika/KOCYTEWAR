ae5837539a4910f531584dfe8c915993f2fc9cdf
reoramalika <reoramalika@gmail.com>
2017-05-30 23:50:08 +0700

    init

---
+++ Assets/Scripts/UIManagerScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManagerScript : MonoBehaviour {
+
+/*	public void ObjectiveMenu()
+	{
+		SceneManager.LoadScene ("objective_menu");
+	}
+
+	public void HowToPlayMenu()
+	{
+		SceneManager.LoadScene ("howtoplay_menu");
+	}
+
+	public void aboutMenu()
+	{
+		SceneManager.LoadScene ("about_menu");
+	}
+
+	public void backtoHelp()
+	{
+		SceneManager.LoadScene ("help_menu");
+	}
+*/
+	public void ChangeToScene(string sceneToChangeTo)
+	{
+		Application.LoadLevel(sceneToChangeTo);
+	}
+
+}
