9345255a930ad796f0aa3ad2f74417d0b01cf809
reoramalika <reoramalika@gmail.com>
2017-05-31 03:04:40 +0700

    update score

---
+++ Assets/Scripts/Gameplay/gameBrain.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameBrain : MonoBehaviour {
+    GameObject prefab, go;
+    public float routinTime,currentTime;
+    private bool stat;
+    enemy enemy;
+    private int score = 0;
+    public GUIText textScore;
+
+    private List<GameObject> enemies;
+    private List<float> enemySpeed;
+
+    // Use this for initialization
+    void Start ()
+    {
+        enemies = new List<GameObject>();
+        enemySpeed = new List<float>();
+        Debug.Log("inserting enemies");
+        enemies.Add(Resources.Load("High Bactery") as GameObject);
+        enemySpeed.Add(0.1f);
+        enemies.Add(Resources.Load("Middle Bactery") as GameObject);
+        enemySpeed.Add(0.1f);
+        enemies.Add(Resources.Load("Low Bactery") as GameObject);
+        enemySpeed.Add(0.1f);
+        Debug.Log(enemies.Count+" enemies inserted");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!stat)
+        {
+            stat = true;
+            StartCoroutine(spawnEnemy(Random.Range(0,enemies.Count),1f));
+        }
+    }
+
+    IEnumerator spawnEnemy(int index, float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        enemy = (enemy)enemies[index].GetComponent("enemy");
+        enemy.moveSpeed = enemySpeed[index];
+        Debug.Log("speed" + enemy.moveSpeed);
+
+        go =Instantiate(enemies[index]);
+        go.transform.position = new Vector2(Random.Range(2.5f, -2.5f),3);
+        stat=false;
+    }
+
+    public void addScore(int value)
+    {
+        score += value;
+        updateScore();
+    }
+
+    private void updateScore()
+    {
+        textScore.text = "Score " + score;
+    }
+}
---
+++ Assets/Scripts/Gameplay/player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+	private Vector3 mousePosition;
+	public float moveSpeed = 0.01f;
+    public int point;
+
+	// Use this for initialization
+	void Start () {
+        point = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton(0)) {
+			mousePosition = Input.mousePosition;
+			mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+			transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+		}
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        point++;
+        Debug.Log("Point: "+point);
+    }
+}
